464bce31fee16ef78d07c0312a144dc140247e9b
Sylvain Kalache <sylvain@kalache.fr>
2024-09-02 19:58:41 +0000

    Closing this method call

--- my-app.py
+++ my-app.py
@@ -10,4 +10,4 @@ def hello():
     return "Hello, World!\n" + "Python version: " + platform.python_version() + "\n"
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=8080
+    app.run(host='0.0.0.0', port=8080)
