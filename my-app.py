229f39e9f7a7bb4d0ac2a183d244d3441058c0fc
Sylvain Kalache <sylvain@kalache.fr>
2024-09-09 20:37:10 +0000

    Matching gunicorn default port

--- my-app.py
+++ my-app.py
@@ -8,6 +8,3 @@ app = Flask(__name__)
 @app.route("/")
 def hello():
     return "Hello, World using pack build!\n" + "Python version: " + platform.python_version() + "\n"
-
-if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=8080)
